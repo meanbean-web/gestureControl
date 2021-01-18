f6bb734fea0a3c7bc7f56edb69a5bc1404955c85
Andreea Bunica <andreeabunica1997@gmail.com>
2021-01-18 13:21:09 +0200

    modified

--- RaspberryPi.py
+++ RaspberryPi.py
@@ -33,89 +33,85 @@ counter = 0
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     counter += 1
     cap = frame.array
-    #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    while True:
+    # while True:
+    #
+    #     ret, frame = cap.read()
+    #     if not ret:
+    #         break
 
-        ret, frame = cap.read()
-        if not ret:
-            break
+    frame = cv2.flip(frame, 1)
 
-        frame = cv2.flip(frame, 1)
+    # Calculate the average FPS
 
-        # Calculate the average FPS
+    frame_counter += 1
+    fps = (frame_counter / (time.time() - start_time))
+    copy = frame.copy()
+    resized_frame = resize(frame, copy)
+    guidelines(frame)
 
-        frame_counter += 1
-        fps = (frame_counter / (time.time() - start_time))
-        copy = frame.copy()
-        resized_frame = resize(frame, copy)
-        guidelines(frame)
+    # print(resized_frame)
 
-        # print(resized_frame)
+    detections = detector(resized_frame)
 
-        detections = detector(resized_frame)
+    if len(detections) > 0:
 
-        if len(detections) > 0:
+        detection = detections[0]
 
-            detection = detections[0]
+        # detection = detections[0]
 
-            # detection = detections[0]
+        x1 = int(detection.left() * scale_factor)
+        y1 = int(detection.top() * scale_factor)
+        x2 = int(detection.right() * scale_factor)
+        y2 = int(detection.bottom() * scale_factor)
 
-            x1 = int(detection.left() * scale_factor)
-            y1 = int(detection.top() * scale_factor)
-            x2 = int(detection.right() * scale_factor)
-            y2 = int(detection.bottom() * scale_factor)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.putText(frame, 'Is it a hand?', (x1, y2 + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
 
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(frame, 'Is it a hand?', (x1, y2 + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+        # Calculate the size of the hand
 
-            # Calculate the size of the hand
+        size = int(x2 - x1 * (y2 - y1))
 
-            size = int(x2 - x1 * (y2 - y1))
+        # Extract the center of the hand on x-axis
+        center_x = int(x1 + (x2 - x1) / 2)
 
-            # Extract the center of the hand on x-axis
-            center_x = int(x1 + (x2 - x1) / 2)
+        # print(center_x)
 
-            # print(center_x)
+        # Display center of hand, for our own sake
 
-            # Display center of hand, for our own sake
+        # cv2.circle(frame, center_x, 1, (0, 255, 0))
 
-            # cv2.circle(frame, center_x, 1, (0, 255, 0))
+        # DECIDE WHETHER IT'S LEFT OR RIGHT
 
-            # DECIDE WHETHER IT'S LEFT OR RIGHT
+        if center_x > right:
+            pyg.press('right')
+            text = 'pressed right'
 
-            if center_x > right:
-                pyg.press('right')
-                text = 'pressed right'
+        elif center_x < left:
+            pyg.press('left')
+            text = 'pressed left'
 
-            elif center_x < left:
-                pyg.press('left')
-                text = 'pressed left'
+        elif size > size_up_th:
+            pyg.press('up')
+            text = 'pressed up'
 
-            elif size > size_up_th:
-                pyg.press('up')
-                text = 'pressed up'
+        elif size < size_down_th:
+            pyg.press('down')
+            text = 'pressed down'
 
-            elif size < size_down_th:
-                pyg.press('down')
-                text = 'pressed down'
+        # if size_down_th
 
-            # if size_down_th
+        image = pyg.screenshot()
 
-            image = pyg.screenshot()
+        img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
-            img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+        # Display the text associated with left/ right detections
+        cv2.putText(frame, text, (220, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (33, 100, 185), 2)
 
-            # Display the text associated with left/ right detections
-            cv2.putText(frame, text, (220, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (33, 100, 185), 2)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-
-
+cap.release()
+cv2.destroyAllWindows()
